bee594265d4a3994cf5d98a9b028601bac2e21ca
Satyam Mittal <satyam101905@gmail.com>
2021-10-21 17:08:06 -0700

    lab 7

--- hoz/Interp1.hs
+++ hoz/Interp1.hs
@@ -598,6 +598,7 @@ sched q n p s@State{front = (tn,st@((m,e):st')):ths, back = back, sas = sas,
   = case exec m e s'' of
       EError msg -> SError msg                                     -- Error
       ESuspend -> trace ("thread supsended: " ++ show st) $ sched q 1 p s{front = ths, back = (tn,st):back}  -- Suspension
+
       ENeed id id2 -> trace ("Needed: "++show id2) $                                                 -- Thunk
         case slookup id sas of
           Nothing -> SError $ "Impossible: store missing id " ++ show id
